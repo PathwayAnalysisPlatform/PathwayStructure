6768938825f52bb4116a5d90d4e80ad41f06d4cf
bramburger <find.bram.burger@gmail.com>
2017-08-23 14:19:56 +0200

    find bottlenecks per year and per pathway

---
+++ R/bottlenecks/perPathway.R
@@ -0,0 +1,71 @@
+centralities <- read.csv("../../Java/PathwayQuery/perPathway170808/centralities.csv", stringsAsFactors=FALSE)
+colnames(centralities)
+nrow(centralities)
+
+topNbetweenness <- function (n) {
+    topBet <- centralities[head(order(centralities$Betweenness,
+                                      decreasing=TRUE), n=n),
+                           c("Protein", "Betweenness")]
+    topBet
+}
+
+topNbetweenness(20)
+
+
+
+
+
+## PER PATHWAY
+## "" is complete thing
+path <- "../../Java/PathwayQuery/perPathway170808/"
+
+allFiles <- list.files(path=path,
+                       pattern="centralities.csv",
+                       full.names=TRUE,
+                       recursive=FALSE)
+length(allFiles)
+
+for (thisFile in allFiles) {
+    pathName <- sub("centralities.csv", "",
+                    sub(path, "",
+                        thisFile))
+    print(pathName)
+    currentFile <- read.csv(thisFile, stringsAsFactors=FALSE)
+    topBet <- currentFile[head(order(currentFile$Betweenness,
+                                     decreasing=TRUE), n=20),
+                          c("Protein", "Betweenness")]
+    print(topBet)
+}
+
+
+## PER YEAR
+path <- "../../Java/PathwayQuery/"
+
+allFiles <- list.files(path=path,
+                       pattern="centralities.csv",
+                       full.names=TRUE,
+                       recursive=FALSE)
+length(allFiles)
+allFiles <-
+    allFiles[-which(startsWith(allFiles, paste0(path, "Out")))]
+allFiles <-
+    allFiles[-which(startsWith(allFiles, paste0(path, "In")))]
+allFiles <-
+    allFiles[-which(startsWith(allFiles, paste0(path, "cen")))]
+length(allFiles)
+
+nrowCurr <- 0
+for (thisFile in allFiles) {
+    pathName <- sub("centralities.csv", "",
+                    sub(path, "",
+                        thisFile))
+    currentFile <- read.csv(thisFile, stringsAsFactors=FALSE)
+    if (nrowCurr < nrow(currentFile)) {
+        nrowCurr <- nrow(currentFile)
+        print(pathName)
+        topBet <- currentFile[head(order(currentFile$Betweenness,
+                                         decreasing=TRUE), n=20),
+                              c("Protein", "Betweenness")]
+        print(topBet)
+    }
+}
